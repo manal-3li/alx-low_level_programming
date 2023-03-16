148b382aebd4ba1ea3d909598522e1072e46ab8c
manal ali <manal.alimoftah@gmail.com>
2023-03-16 16:35:20 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Description: A C program that prints with function
+ * Description: A C program that prints with function
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
 */
 
-int main ()
+int main(void)
 {
 	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
