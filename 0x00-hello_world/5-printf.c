dea100acd2349c13e82d40d645fa19653c70e534
manal ali <manal.alimoftah@gmail.com>
2023-03-16 16:42:08 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0); 
+}
+
