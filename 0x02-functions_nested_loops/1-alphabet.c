50456db504c338f03cab714b2671710970f7a638
manal ali <manal.alimoftah@gmail.com>
2023-03-27 02:35:55 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,31 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- * followed by a new line
- * Return: Always 0 (Success)
- */
-int main(void)
+* print_alphabet -> prints the lowercase alphabets
+*/
+
+void print_alphabet(void)
 {
-	long int i, j, k, next;
+	int j;
 
-	j = 1;
-
-	k = 2;
-
-	for (i = 1; i <= 50; ++i)
+	for (j = 'a'; j <= 'z'; j++)
 	{
-		if (j != 20365011074)
-		{
-			printf("%ld, ", j);
-		} else
-		{
-			printf("%ld\n", j);
-		}
-		next = j + k;
-		j = k;
-		k = next;
+		_putchar(j);
 	}
-
-	return (0);
+	_putchar('\n');
 }
