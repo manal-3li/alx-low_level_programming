7427fe38269239dfb2e520286f4013d3c00c1a4b
manal ali <manal.alimoftah@gmail.com>
2023-04-19 17:07:49 -0700

    a

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,13 @@
 /**
  * print_name - print name using pointer to function
  * @name: string to add
- * @m: pointer to function
+ * @f: pointer to function
  * Return: nothing
  **/
-void print_name(char *name, void (*m)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || m == NULL)
+	if (name == NULL || f == NULL)
 		return;
 
-	m(name);
+	f(name);
 }
