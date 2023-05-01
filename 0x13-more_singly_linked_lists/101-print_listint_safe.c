54dc9cc5724c55f4858c3bb9f42f6c69808b9129
manal-3li <manal.alimoftah@gmail.com>
2023-05-01 08:53:18 -0700

    a

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,25 +10,25 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *current = head;
-    const listint_t *prev = NULL;
-    size_t count = 0;
+const listint_t *current = head;
+const listint_t *prev = NULL;
+size_t count = 0;
 
-    while (current != NULL)
-    {
-        printf("[%p] %d\n", (void *)current, current->n);
-        count++;
+while (current != NULL)
+{
+printf("[%p] %d\n", (void *)current, current->n);
+count++;
 
-        /* Check if we've visited this node before */
-        if (current < prev)
-        {
-            fprintf(stderr, "Error: linked list is a loop\n");
-            exit(98);
-        }
+/* Check if we've visited this node before */
+if (current < prev)
+{
+fprintf(stderr, "Error: linked list is a loop\n");
+exit(98);
+}
 
-        prev = current;
-        current = current->next;
-    }
+prev = current;
+current = current->next;
+}
 
-    return count;
+return (count);
 }
