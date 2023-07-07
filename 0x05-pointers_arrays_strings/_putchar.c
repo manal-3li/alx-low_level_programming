8eac536742e724adaee29251fc8d6643c0f3fde6
manal-3li <manal.alimoftah@gmail.com>
2023-07-07 09:15:00 -0700

    a

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,13 +1,5 @@
-#include <unistd.h>
-
-/**
-* _putchar - writes the character c to stdout
-* 0c: The character to print 
-*
-* Returm: On success 1.
-* On error, -1 is returned, and error is set appropriately.
-*/
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+    /* TODO: Implement function */
+return (0);
 }
