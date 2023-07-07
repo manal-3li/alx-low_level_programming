f1fd3007522a17f3f47743fa2cd2c42db33010b7
manal-3li <manal.alimoftah@gmail.com>
2023-07-07 15:58:09 -0700

    a

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,14 @@
 
 /**
 * main - program should print a number, followed by a new line
-* @argc: int
-*
-* Return: 0
+* @argc:int
+* @argv:pointer
+* Return:0
 */
 
-int main(int argc)
+int main(int argc, char **argv)
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
+	(void)args;
 	return (0);
 }
