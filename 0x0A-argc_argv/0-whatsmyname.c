860c28c38e0ad41a3c52b497bf5bd1aa9f16c09e
manal-3li <manal.alimoftah@gmail.com>
2023-07-07 15:20:43 -0700

    name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - program to prints its name, followed by a new line
+* @argc: int
+* @argv: char
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	printf("%s", argv[argc - 1]);
+	(void)argc;
+	return (0);
+}
