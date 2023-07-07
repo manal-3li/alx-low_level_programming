8f4adfc40aa3cdec6098055b53c980730920d9e6
manal-3li <manal.alimoftah@gmail.com>
2023-07-07 15:54:07 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - program should print a number, followed by a new line
+* @argc: int
+*
+* Return: 0
+*/
+
+int main(int argc)
+{
+	printf("%d\n", argc);
+	return (0);
+}
